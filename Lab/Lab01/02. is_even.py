f62adc2d16d540b3647266c8bd114eb5c7465319
pprwf <pprwfwithsleeping.pills@gmail.com>
2023-02-11 15:57:23 +0700

    Lab | Week 01 (is_even)

--- Lab/Lab01/02. is_even.py
+++ Lab/Lab01/02. is_even.py
@@ -1,7 +1,8 @@
-''''''
+'''Lab 1.2 Check even number'''
 
 def is_even(k_num):
     '''fuq pep8'''
-    while k_num > 0: k_num -= 2
+    while k_num > 0:
+        k_num -= 2
     return True if k_num == 0 else False
-print(is_even(int(input())))
+print(is_even(abs(int(input()))))
