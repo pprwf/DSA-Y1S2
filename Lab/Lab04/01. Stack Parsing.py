4e0a7cffd6e7a4f66d45b23210242c89f963f883
pprwf <pprwfwithsleeping.pills@gmail.com>
2023-02-12 14:42:02 +0700

    Lab | Week 04 (Stack Parsing)

--- Lab/Lab04/01. Stack Parsing.py
+++ Lab/Lab04/01. Stack Parsing.py
@@ -5,15 +5,11 @@ class ArrayStack:
     def __init__(self):
         self.data = []
 
-    def size(self):
-        print("Size of Stack :", len(self.data), "\n----------")
-
     def is_empty(self):
-        print("This stack is empty? : ", end="")
         return self.data == []
 
     def push(self, data):
-        print("Push (%s) : " %data, end="")
+        print("Push \"%s\" : " %data, end="")
         self.data.append(data)
         self.printStack()
 
@@ -22,32 +18,33 @@ class ArrayStack:
             print("Underflow: Cannot pop data from an empty list")
         else:
             value = self.data[-1]
-            print("Pop (%s) : " %value, end="")
+            print("Pop  \"%s\" : " %value, end="")
             self.data.pop()
             self.printStack()
             return value
 
-    def stackTop(self):
-        return None if self.data == [] else self.data[-1]
-
     def printStack(self):
-        print(self.data, "\n----------")
-
-# from Lab.Lab03.Stack import ArrayStack
-
+        print(self.data)
 
 def is_parentheses_matching(expression):
     parsingStack = ArrayStack()
+    print("----------")
     for i in expression:
         if i == "(":
+            print("Read ( then ", end="")
             parsingStack.push(i)
         elif i == ")":
+            if parsingStack.data == []:
+                print("----------\nParentheses in", expression, "are unmatched")
+                return False
+            print("Read ) then ", end="")
             parsingStack.pop()
+    print("----------")
     if parsingStack.data != []:
         print("Parentheses in", expression, "are unmatched")
     return parsingStack.is_empty()
 
-parsingStack = ArrayStack()
 str = "(((A - B) * C)"
 result = is_parentheses_matching(str)
 print(result)
+print("----------")
